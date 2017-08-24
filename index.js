e694a5151a2e43ba15dac71b1792201f8aa5f9d4
DanielNMartin <dan.martin@sainsburys.co.uk>
2017-08-24 15:40:13 +0100

    Function Structured with 3 Parameters.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+
+function dateRangeAnaliser(startDate, endDate, step ) {
 var listDate = [];
-var endDate = new Date('2019-01-01');
-var startDate = new Date('2017-01-01');
 while (startDate.getTime() < endDate.getTime()) {
-  startDate.setDate(startDate.getDate() + 41);
+  startDate.setDate(startDate.getDate() + step);
   var tempDate = new Date(startDate.getTime());
   var stringDate = tempDate.getDate() + ", " + (tempDate.getMonth() + 1) + ", " + tempDate.getFullYear() + " "
   listDate.push(stringDate);
 };
 
-console.log(listDate);
+ return listDate;
+}
+
+/*
+var startDate = new Date('2017-01-01');
+var endDate = new Date('2019-01-01');
+console.log(dateRangeAnaliser(startDate, endDate, 41));
+
+var startDate1 = new Date('2020-01-01');
+var endDate1 = new Date('2022-01-01');
+console.log(dateRangeAnaliser(startDate1, endDate1, 10));
+*/
